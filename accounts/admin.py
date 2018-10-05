3659357f10615dea5204149b8ca6b958c2cce321
Ochui Princewill <ochui.princewill@gmail.com>
2018-10-05 13:52:58 +0100

    extends django user model with custom fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email', 'phone_number_status',  'country']
+    list_editable = ['phone_number_status']
+    list_filter = ['date_joined', 'last_login', 'is_active', 'is_staff']
+    model = CustomUser
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    
+    PHONE_NUMBER_STATUS_CHOICES = (
+        ('0', 'NOT VERIFIED'),
+        ('1', 'VERIFIED')
+    )
+
+    avatar = models.ImageField(_("avatar"), upload_to='users/%Y/%m/%d/', blank=True)
+    country = models.CharField(_("country"), max_length=50)
+    phone_number = models.CharField(_("phone number"), max_length=20, null=True, blank=True)
+    phone_number_status = models.CharField(_("phone number status"), max_length=50, choices=PHONE_NUMBER_STATUS_CHOICES, default='0')
+
